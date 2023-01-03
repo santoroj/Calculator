73cb44a78a5644ebe4450d4dad105cc3a0159b33
Joe Santoro <Joe.Santoro@unix-consultants.co.uk>
2023-01-03 15:48:16 +0000

    Updated versions

--- test_caclculator.py
+++ test_caclculator.py
@@ -2,14 +2,13 @@
 Unit test for the calculator library
 """
 
-import calculator 
+import calculator
 
-class TestCalculator: 
-    
+
+class TestCalculator:
+
     def test_addition(self):
-        assert 4 == calculator.add(2,2)
-    
-    
+        assert 4 == calculator.add(2, 2)
+
     def test_subtraction(self):
-        assert 2 == calculator.subtract(4,2)
-        
+        assert 2 == calculator.subtract(4, 2)
