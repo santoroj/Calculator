2a3422651fc1645cbead1af213dd669239abe1d3
Joe Santoro <Joe.Santoro@unix-consultants.co.uk>
2023-01-03 14:44:14 +0000

    Unit tests for calculator

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
  python -m venv calculator 
  . calculator/bin/activate
 
+Install:
+pip install flake8 pytest pytest-cov 
+
+pip freeze > requirements.txt
+cat requirements.txt
+
+### Check if anything is wrong
+flake8 --statistics
+
+### To test the code
+pytest  -v --cov .
--- calculator.py
+++ calculator.py
@@ -1,10 +1,11 @@
-    """
+"""
     Calculator library contain basic math operations.
-    """
-    def add(first_term, second_term):
-        return first_term + second_term 
+"""
+def add(first_term, second_term):
+    return first_term + second_term 
     
-    def subtract(first_term, second_term):
-        return first_term - second
+    
+def subtract(first_term, second_term):
+    return first_term - second_term
     
     
