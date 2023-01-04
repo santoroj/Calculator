98ae9d1380ca0e8a7b077d978c17f248a02b2486
Joe Santoro <Joe.Santoro@unix-consultants.co.uk>
2023-01-04 11:07:50 +0000

    Testing github actions

--- test_caclculator.py
+++ test_caclculator.py
@@ -8,7 +8,7 @@ import calculator
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == calculator.add(2, 2)
+        assert 8 == calculator.add(4, 4)
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
