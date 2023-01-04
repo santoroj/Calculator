bd5527ae5e7f78a382535da3682b6892c6d9f0f1
Joe Santoro <Joe.Santoro@unix-consultants.co.uk>
2023-01-04 11:25:08 +0000

    Testing github actions with comments this time

--- calculator.py
+++ calculator.py
@@ -4,16 +4,20 @@
 
 
 def add(first_term, second_term):
+    """ Add test """
     return first_term + second_term
 
 
 def subtract(first_term, second_term):
+    """ subtract test """
     return first_term - second_term
 
 
 def multiply(first_term, second_term):
+    """ multiply test """
     return first_term * second_term
 
 
 def division(first_term, second_term):
+    """ divide test """
     return first_term / second_term
--- test_caclculator.py
+++ test_caclculator.py
@@ -6,15 +6,19 @@ import calculator
 
 
 class TestCalculator:
-
+    """  test """
     def test_addition(self):
+        """ Add test """
         assert 10 == calculator.add(4, 6)
 
     def test_subtraction(self):
+        """ subtraction test """
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
+        """ multiply test """
         assert 100 == calculator.multiply(10, 10)
 
     def test_division(self):
+        """ division test """
         assert 10 == calculator.division(100, 10)
